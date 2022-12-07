65c5d0cda2acb2e387c0c8108ea2e1f7e649eb75
Francesco Zollo <francescozollo@rocketmail.com>
2022-12-07 18:53:33 +0100

    Aggiunto return per compilazione
    - Francesco Zollo (07/12/2022)

--- app/src/main/java/com/example/pregaverso/Database.kt
+++ app/src/main/java/com/example/pregaverso/Database.kt
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class Database(context : Context) : SQLiteOpenHelper(context ,NOME_DATABASE, null, VERSIONE_DATABASE){
     override fun onCreate(db: SQLiteDatabase?) {
-
+        return
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
