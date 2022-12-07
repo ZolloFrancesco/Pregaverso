c9ac99349b4dea664c1eb010e28b687b20aebf30
Francesco Zollo <francescozollo@rocketmail.com>
2022-12-07 17:13:07 +0100

    Creato file costanti.kt, aggiunte le costanti che rappresentano:  -le stringhe d'intestazione delle tabelle del database  -i nomi degli attributi di tutte le tabelle
    - Francesco Zollo (07/12/2022)

---
+++ app/src/main/java/com/example/pregaverso/costanti.kt
@@ -0,0 +1,29 @@
+package com.example.pregaverso
+
+const val DATABASE_VERSION = 0
+const val DATABASE_NAME = "Pregaverso.db"
+
+
+const val NOME_TABELLA_PLEBEI = "Plebei"
+const val PLEBEI_NOME = "Nome"
+const val PLEBEI_CASATA = "Casata"
+const val PLEBEI_BAIOCCHI = "Baiocchi"
+
+const val NOME_TABELLA_SACERDOTI = "Sacerdoti"
+const val SACERDOTI_NOME = "Nome"
+const val SACERDOTI_DIOCESI = "Diocesi"
+
+const val NOME_TABELLA_MIRACOLI = "Miracoli"
+const val MIRACOLI_DESCRIZIONE = "Descrizione"
+const val MIRACOLI_NOMESANTO = "NomeSanto"
+const val MIRACOLI_COSTO = "Costo"
+
+const val NOME_TABELLA_COMMENTIMIRACOLI = "CommentiMiracoli"
+const val COMMENTIMIRACOLI_DESCRIZIONE = "Descrizione"
+const val COMMENTIMIRACOLI_NOMESANTO = "NomeSanto"
+const val COMMENTIMIRACOLI_COMMENTO = "Commento"
+
+const val NOME_TABELLA_LOGIN = "Login"
+const val LOGIN_NOME = "Nome"
+const val LOGIN_CASATADIOCESI = "Casata-Diocesi"
+const val LOGIN_PAROLADORDINE = "ParolaDOrdine"
