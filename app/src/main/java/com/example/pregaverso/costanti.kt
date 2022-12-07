cc540e097d940e4242c5bd29092ed1854147a8e7
Francesco Zollo <francescozollo@rocketmail.com>
2022-12-07 17:59:06 +0100

    Aggiunti commenti esplicativi
    - Francesco Zollo (07/12/2022)

--- app/src/main/java/com/example/pregaverso/costanti.kt
+++ app/src/main/java/com/example/pregaverso/costanti.kt
@@ -3,26 +3,30 @@ package com.example.pregaverso
 const val VERSIONE_DATABASE = 0
 const val NOME_DATABASE = "Pregaverso.db"
 
-
+// costanti della tabella Plebei
 const val NOME_TABELLA_PLEBEI = "Plebei"
 const val PLEBEI_NOME = "Nome"
 const val PLEBEI_CASATA = "Casata"
 const val PLEBEI_BAIOCCHI = "Baiocchi"
 
+// costanti della tabella Sacerdoti
 const val NOME_TABELLA_SACERDOTI = "Sacerdoti"
 const val SACERDOTI_NOME = "Nome"
 const val SACERDOTI_DIOCESI = "Diocesi"
 
+// costanti della tabella Miracoli
 const val NOME_TABELLA_MIRACOLI = "Miracoli"
 const val MIRACOLI_DESCRIZIONE = "Descrizione"
 const val MIRACOLI_NOMESANTO = "NomeSanto"
 const val MIRACOLI_COSTO = "Costo"
 
+// costanti della tabella CommentiMiracoli
 const val NOME_TABELLA_COMMENTIMIRACOLI = "CommentiMiracoli"
 const val COMMENTIMIRACOLI_DESCRIZIONE = "Descrizione"
 const val COMMENTIMIRACOLI_NOMESANTO = "NomeSanto"
 const val COMMENTIMIRACOLI_COMMENTO = "Commento"
 
+// costanti della tabella Login
 const val NOME_TABELLA_LOGIN = "Login"
 const val LOGIN_NOME = "Nome"
 const val LOGIN_CASATADIOCESI = "Casata-Diocesi"
