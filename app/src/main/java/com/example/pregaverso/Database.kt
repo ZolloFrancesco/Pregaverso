f932b966ee67b0d1fecb8cc69b24d66a0856aecc
Zollo <francescozollo@rocketmail.com>
2022-12-08 13:26:08 +0100

    Aggiunte funzioni prendiSacerdoti, aggiungiBaiocchiAPlebeo, aggiungiPlebeo e eliminaPlebeo
    - Francesco Zollo (08/12/2022)

--- app/src/main/java/com/example/pregaverso/Database.kt
+++ app/src/main/java/com/example/pregaverso/Database.kt
@@ -152,4 +152,128 @@ class Database(context : Context) : SQLiteOpenHelper(context ,NOME_DATABASE, nul
         return listaDaRestituire
     }
 
+    // Restituisce la lista di tutti i Sacerdoti presenti al momento della chiamata all'interno del Database.
+    // Se non ci sono Sacerdoti, restituisce una lista di Sacerdoti vuota.
+    fun prendiSacerdoti() : ArrayList<Sacerdote>{
+
+        val db : SQLiteDatabase = readableDatabase
+
+        val listaDaRestituire : ArrayList<Sacerdote> = ArrayList()
+
+        val comando = "SELECT * FROM $NOME_TABELLA_SACERDOTI"
+        val cursore : Cursor = db.rawQuery(comando,null)
+
+        if(cursore.moveToFirst()){
+            do {
+                val sacerdotePrelevato = Sacerdote()
+
+                sacerdotePrelevato.nome = cursore.getString(0)
+                sacerdotePrelevato.diocesi = cursore.getString(1)
+
+                listaDaRestituire.add(sacerdotePrelevato)
+            } while(cursore.moveToNext())
+        }
+
+        cursore.close()
+
+        return listaDaRestituire
+    }
+
+    // aggiunge nBaiocchi al Plebeo univocamente identificato dalla coppia {nomePlebeo,casata} all'interno del Database.
+    // restituisce false se non ha modificato nessun Plebeo, se ha modificato troppi Plebei o se c'e' stato un errore sconosciuto.
+    // restituisce true in caso di corretta modifica.
+    fun aggiungiBaiocchiAPlebeo(nomePlebeo : String, casata : String, nBaiocchi : Int) : Boolean{
+
+        if(nBaiocchi < 0 || nomePlebeo == "" || casata == ""){
+            Log.d("AGGIUNGO $nBaiocchi BAIOCCHI A $nomePlebeo DI CASA $casata","INPUT NON VALIDI")
+            return false
+        }
+
+        val db : SQLiteDatabase = writableDatabase
+
+        val listaPlebei = prendiPlebei()
+
+        var baiocchiVecchi = 0
+
+        for(i in 0 until listaPlebei.size){
+
+            if( ( listaPlebei[i].nome == nomePlebeo ) && ( listaPlebei[i].casata == casata ) ){
+
+                baiocchiVecchi = listaPlebei[i].baiocchi
+            }
+        }
+
+        val modificato = ContentValues()
+
+        modificato.put(PLEBEI_NOME,nomePlebeo)
+        modificato.put(PLEBEI_CASATA,casata)
+        modificato.put(PLEBEI_NOME,baiocchiVecchi + nBaiocchi)
+
+        val nModificati = db.update(NOME_TABELLA_PLEBEI, modificato, "$PLEBEI_NOME=? AND $PLEBEI_CASATA=?", arrayOf(nomePlebeo,casata))
+
+        if (nModificati == 1){
+            Log.d("AGGIUNGO $nBaiocchi BAIOCCHI A $nomePlebeo DI CASA $casata", "SUCCESSO")
+            return true
+        }
+
+        if(nModificati == 0){
+            Log.d("AGGIUNGO $nBaiocchi BAIOCCHI A $nomePlebeo DI CASA $casata", "NESSUN PLEBEO MODIFICATO")
+            return false
+        }
+
+        if(nModificati > 1){
+            Log.d("AGGIUNGO $nBaiocchi BAIOCCHI A $nomePlebeo DI CASA $casata", "TROPPI PLEBEI MODIFICATI!")
+            return false
+        }
+        Log.d("AGGIUNGO $nBaiocchi BAIOCCHI A $nomePlebeo DI CASA $casata", "ERRORE SCONOSCIUTO")
+        return false
+    }
+
+    // inserisce un nuovo Plebeo con nome nome, casata casata e con un numero di Baiocchi pari a nBaiocchi.
+    // restituisce false non sono stati passati input validi.
+    // restituisce true in caso di corretto inserimento.
+    fun aggiungiPlebeo(nome : String, casata : String, nBaiocchi : Int) : Boolean{
+
+        if(nBaiocchi < 0 || nome == "" || casata == ""){
+            Log.d("INSERIMENTO DI $nome DI CASA $casata","INPUT NON VALIDI")
+            return false
+        }
+
+        val db = writableDatabase
+
+        val daAggiungere = ContentValues()
+        daAggiungere.put(PLEBEI_NOME,nome)
+        daAggiungere.put(PLEBEI_CASATA,casata)
+        daAggiungere.put(PLEBEI_BAIOCCHI,nBaiocchi)
+
+        db?.insert(NOME_TABELLA_PLEBEI, null, daAggiungere)
+
+        Log.d("INSERIMENTO DI $nome DI CASA $casata","SUCCESSO")
+        return true
+    }
+
+    // elimina un Plebeo con nome nome e casata casata dal Database.
+    // restituisce false se gli input non sono validi, se non ha eliminato niente o se ha eliminato piu' di una riga.
+    // restituisce true in caso di eliminazione di un Plebeo (non assicura la correttezza della scelta dell'eliminato).
+    fun eliminaPlebeo(nome : String, casata : String) : Boolean{
+        if(nome == "" || casata == "") return false
+
+        val db = writableDatabase
+
+        val nRigheEliminate = db.delete(NOME_TABELLA_PLEBEI,"$PLEBEI_NOME=? AND $PLEBEI_CASATA=?", arrayOf(nome,casata))
+
+        if(nRigheEliminate == 0){
+            Log.d("ELIMINO $nome DI CASA $casata","NESSUNA RIGA ELIMINATA")
+            return false
+        }
+
+        if(nRigheEliminate>1){
+            Log.d("ELIMINO $nome DI CASA $casata","ELIMINATE PIU' PLEBEI!")
+            return false
+        }
+
+        Log.d("ELIMINO $nome DI CASA $casata","SUCCESSO")
+        return true
+    }
+
 }
