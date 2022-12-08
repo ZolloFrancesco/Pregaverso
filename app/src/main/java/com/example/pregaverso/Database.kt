73ce9c27f38be481e5db7d40ae2ef5e244d59063
Zollo <francescozollo@rocketmail.com>
2022-12-08 13:22:21 +0100

    Import necessari al corretto funzionamento dell'applicazione
    - Francesco Zollo (08/12/2022)

--- app/src/main/java/com/example/pregaverso/Database.kt
+++ app/src/main/java/com/example/pregaverso/Database.kt
@@ -1,9 +1,11 @@
 package com.example.pregaverso
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 /*
 +--------------------------------------------------+
