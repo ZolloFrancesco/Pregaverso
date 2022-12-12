af45c487fe02b003b037fb2b21456c9c3b50eca6
Zollo <francescozollo@rocketmail.com>
2022-12-12 12:19:42 +0100

    Test rapidi
    - Tutti (12/12/2022)

--- app/src/main/java/com/example/pregaverso/MainActivity.kt
+++ app/src/main/java/com/example/pregaverso/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.pregaverso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Database(this)
+
+
+        db.aggiungiPlebeo("marco","imbecilli",30)
+        db.aggiungiPlebeo("lorenzo","forti",100)
+
+        val x = db.prendiPlebei()
+
+        Log.d("QUALCOSA","${x[0].nome} ${x[0].casata} ${x[0].baiocchi}")
+        Log.d("QUALCOSA","${x[1].nome} ${x[1].casata} ${x[1].baiocchi}")
+
     }
 }
