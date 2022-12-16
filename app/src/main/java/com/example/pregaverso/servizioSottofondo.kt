1ca0461ed48ecb4b64bbbf7b40658450a3d83d43
Francesco Zollo <francescozollo@rocketmail.com>
2022-12-16 15:16:33 +0100

    Implementazione del service
    - Francesco Zollo (15/12/2022)

--- app/src/main/java/com/example/pregaverso/servizioSottofondo.kt
+++ app/src/main/java/com/example/pregaverso/servizioSottofondo.kt
@@ -2,11 +2,21 @@ package com.example.pregaverso
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
 
 class ServizioSottofondo : Service() {
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val mediap = MediaPlayer.create(this, R.raw.sottofondostoria)
+
+        mediap.setLooping(true)
+
+
+        return super.onStartCommand(intent, flags, startId)
     }
 
 
