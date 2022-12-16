8a25c72c06e9e29dff39bb30ef3552637a069d7d
Francesco Zollo <francescozollo@rocketmail.com>
2022-12-16 18:57:40 +0100

    Aggiunto Service e subito riscritto in java perchè in kotlin non era simpatico
    - Francesco Zollo (16/12/2022)

---
+++ app/src/main/java/com/example/pregaverso/ServizioSottofondo.java
@@ -0,0 +1,35 @@
+package com.example.pregaverso;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class ServizioSottofondo extends Service {
+
+    MediaPlayer mediap;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        mediap = MediaPlayer.create(this, R.raw.sottofondostoria);
+        mediap.setLooping(true);
+        mediap.start();
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    @Override
+    public void onDestroy(){
+        mediap.stop();
+        super.onDestroy();
+    }
+
+
+}
--- app/src/main/java/com/example/pregaverso/servizioSottofondo.kt
+++ app/src/main/java/com/example/pregaverso/servizioSottofondo.kt
@@ -4,20 +4,31 @@ import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.IBinder
+/*
 
 class ServizioSottofondo : Service() {
+    private var mediap : MediaPlayer = MediaPlayer.create(this, R.raw.sottofondostoria)
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val mediap = MediaPlayer.create(this, R.raw.sottofondostoria)
-
-        mediap.setLooping(true)
-
-
+        mediap.isLooping = true
+        mediap.start()
         return super.onStartCommand(intent, flags, startId)
     }
 
+    override fun stopService(name: Intent?): Boolean {
+        mediap.stop()
+        return super.stopService(name)
+    }
 
+    override fun onDestroy() {
+        mediap.stop()
+        super.onDestroy()
+    }
 }
+ */
+
+
