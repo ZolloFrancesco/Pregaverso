d02f9a3fb7986ef503fbd444d1d30b968c3f654e
Zollo <francescozollo@rocketmail.com>
2022-12-16 12:05:34 +0100

    Implementazione del recycler view
    - Francesco Zollo (16/12/2022)

--- app/src/main/java/com/example/pregaverso/HomeSacerdote.kt
+++ app/src/main/java/com/example/pregaverso/HomeSacerdote.kt
@@ -1,11 +1,115 @@
 package com.example.pregaverso
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.cardmiracolosacerdote.view.*
+import kotlinx.android.synthetic.main.home_sacerdote.*
+import kotlinx.android.synthetic.main.popupinserimentomiracolo.view.*
 
 class HomeSacerdote : AppCompatActivity() {
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_sacerdote)
+
+        // nascondo la ActionBar per estetica
+        supportActionBar?.hide()
+
+        // dichiaro un handler per database per eseguire le operazioni messe a disposizione
+        val db = Database(this)
+
+        // dichiaro un array di task
+        val listaMiracoli = ArrayList<Miracolo>()
+
+        // dichiaro un adapter che gestisce la visualizzazione di todoListItem
+        val adapter = AdattatoreMiracoli(listaMiracoli, this)
+        recyclerView.adapter = adapter
+
+        // dichiaro un LinearLayoutManager
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        // helper che permette di intercettare uno swipe
+        ItemTouchHelper(
+
+            // oggetto che intercetta sia gli swipe verso destra che quelli verso sinistra
+            object :
+                ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+                // non sovrascrivo la funzione onMove()
+                override fun onMove(
+                    rw: RecyclerView,
+                    vh: RecyclerView.ViewHolder,
+                    trg: RecyclerView.ViewHolder
+                ) = false
+
+                override fun onSwiped(vh: RecyclerView.ViewHolder, dir: Int) {
+
+                    // se c'è stato uno swipe verso sinistra
+                    if (dir == ItemTouchHelper.LEFT) {
+
+                        // chiamo il metodo che prende come argomento l'indice dell'elemento
+                        // che verrà eliminato dal viewRecycler (aggiorna anche il database)
+                        adapter.cancellaByPosition(vh.adapterPosition)
+
+                    }
+                }
+            }
+
+            // il touchHelper lavora sulla recyclerView
+        ).attachToRecyclerView(recyclerView)
+
+
+        // listener per il pulsante flottante
+        btnAggiungiMiracolo.setOnClickListener {
+
+            val dialogBuilder: AlertDialog.Builder?
+            val dialog: AlertDialog?
+            val view =
+                LayoutInflater.from(this).inflate(R.layout.popupinserimentomiracolo, null, false)
+            val btnInserisci = view.btnInserisci
+            dialogBuilder = AlertDialog.Builder(this).setView(view)
+            dialog = dialogBuilder!!.create()
+            dialog.show()
+            btnInserisci.setOnClickListener {
+                if (!TextUtils.isEmpty(view.testoInserito.text.toString())) {
+                    db.aggiungiMiracoli(
+                        view.descrInserita.text.toString(),
+                        view.nomesanto.text.toString(),
+                        view.costoInserito.text.toString().toInt(),
+                        view.testoInserito.text.toString()
+                    )
+                    adapter!!.notifyItemInserted(adapter!!.miracoli.size)
+                    dialog!!.dismiss()
+                }
+            }
+
+            // lista che contiene tutti i miracoli nel database
+            val miracoliNelDb = db.prendiMiracoli()
+
+            // per ogni elemento dell'array todoList
+            for (i in 0 until miracoliNelDb.size) {
+
+                // istanzio un nuovo task
+                val nuovo = Miracolo()
+
+                // lo riempio con l'elemento i-esimo di todoList
+                nuovo.testo = miracoliNelDb[i].testo
+                nuovo.nomeSanto = miracoliNelDb[i].nomeSanto
+                nuovo.descr = miracoliNelDb[i].descr
+                nuovo.costo = miracoliNelDb[i].costo
+
+                // aggiungo il nuovo task alla lista che viene visualizzata nel recyclerView
+                listaMiracoli.add(nuovo)
+            }
+        }
     }
 }
