d5fb13d67cb8ff629f7adc3109e4024165b2c1d5
Zollo <francescozollo@rocketmail.com>
2022-12-16 13:12:54 +0100

    Aggiunta classe ServizioSottoFondo
    - Francesco Zollo (16/12/2022)

---
+++ app/src/main/java/com/example/pregaverso/servizioSottofondo.kt
@@ -0,0 +1,13 @@
+package com.example.pregaverso
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ServizioSottofondo : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+
+}
