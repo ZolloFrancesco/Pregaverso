9f27ffca9d603437b091ace4cbfd82e2b215362d
Zollo <francescozollo@rocketmail.com>
2022-12-18 16:54:45 +0100

    Rimosse import inutili e corretto bug sugli id
    - Francesco Zollo (18/12/2022)

--- app/src/main/java/com/example/pregaverso/HomePlebeo.kt
+++ app/src/main/java/com/example/pregaverso/HomePlebeo.kt
@@ -1,23 +1,15 @@
 package com.example.pregaverso
 
 import android.app.AlertDialog
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.animation.AnimationUtils
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.cardviewmiracoliplebeo.*
-import kotlinx.android.synthetic.main.home_sacerdote.*
+import kotlinx.android.synthetic.main.cardviewmiracoloplebeo.*
+import kotlinx.android.synthetic.main.homesacerdote.*
 import kotlinx.android.synthetic.main.homeplebeo.*
-import kotlinx.android.synthetic.main.popupbaiocchisacerdote.*
-import kotlinx.android.synthetic.main.popupbaiocchisacerdote.view.*
 import kotlinx.android.synthetic.main.popupinserimentomiracolo.view.*
-
-var plebeoCorrente = Plebeo()
 
 class HomePlebeo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,23 +27,10 @@ class HomePlebeo : AppCompatActivity() {
 
         // dichiaro un adapter che gestisce la visualizzazione di todoListItem
         val adapter = AdattatoreMiracoliPlebeo(listaMiracoli, this)
-        recyclerView.adapter = adapter
+        recyclerViewPlebeo.adapter = adapter
 
         // dichiaro un LinearLayoutManager
         val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
-
-        btnCompra.setOnClickListener {
-
-            val bounce = AnimationUtils.loadAnimation(applicationContext,R.anim.bounce)
-            btnCompra.startAnimation(bounce)
-
-            if(floatBaiocchi.text.toString().toInt() > baiocchiNecessari.text.toString().toInt()){
-                floatBaiocchi.startAnimation(bounce)
-                floatBaiocchi.text = (floatBaiocchi.text.toString().toInt() - baiocchiNecessari.text.toString().toInt()).toString()
-                //adapter.cancellaByPosition(adapter.adapterPosition)
-            }
-        }
-
+        recyclerViewPlebeo.layoutManager = layoutManager
     }
 }
