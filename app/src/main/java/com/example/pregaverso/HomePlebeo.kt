b0dffed7878f1abc2ec359cfd28c7d3d5494e02d
Zollo <francescozollo@rocketmail.com>
2022-12-19 00:15:54 +0100

    Aggiunto popup per trappola nella HomePlebeo
    - Francesco Zollo (18/12/2022)

--- app/src/main/java/com/example/pregaverso/HomePlebeo.kt
+++ app/src/main/java/com/example/pregaverso/HomePlebeo.kt
@@ -1,15 +1,16 @@
 package com.example.pregaverso
 
 import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.cardviewmiracoloplebeo.*
-import kotlinx.android.synthetic.main.homesacerdote.*
 import kotlinx.android.synthetic.main.homeplebeo.*
 import kotlinx.android.synthetic.main.popupinserimentomiracolo.view.*
+import kotlinx.android.synthetic.main.popuptrappola.*
+import kotlinx.android.synthetic.main.popuptrappola.view.*
 
 class HomePlebeo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,5 +33,24 @@ class HomePlebeo : AppCompatActivity() {
         // dichiaro un LinearLayoutManager
         val layoutManager = LinearLayoutManager(this)
         recyclerViewPlebeo.layoutManager = layoutManager
+
+        val dialogBuilder: AlertDialog.Builder?
+        val dialog: AlertDialog?
+        val view = LayoutInflater.from(this).inflate(R.layout.popuptrappola, null, false)
+        val btnGiura = view.btnGiura
+        dialogBuilder = AlertDialog.Builder(this).setView(view)
+        dialog = dialogBuilder!!.create()
+        dialog.show()
+        btnGiura.setOnClickListener {
+                dialog!!.dismiss()
+                //if(bugiaTrappola.text.toString().toInt() != baiocchiPassati){
+                    startActivity(Intent(this@HomePlebeo, Trappola::class.java))
+                //}
+            }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        Log.d("NON SI PUO'","FREGATO")
     }
 }
