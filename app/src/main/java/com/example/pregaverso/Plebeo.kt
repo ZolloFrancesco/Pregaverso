a2cbe63948d705bc0609e032000fee1210026d90
Zollo <francescozollo@rocketmail.com>
2022-12-18 16:57:10 +0100

    Aggiunte funzioni daiBaiocchi, togliBaiocchi e commentaMiracolo
    - Francesco Zollo (18/12/2022)

--- app/src/main/java/com/example/pregaverso/Plebeo.kt
+++ app/src/main/java/com/example/pregaverso/Plebeo.kt
@@ -1,23 +1,35 @@
 package com.example.pregaverso
 
+import android.content.Context
+
 class Plebeo {
     var nome = ""
     var casata = ""
     var baiocchi = 0
 
-    fun daiBaiocchi(nBaiocchi : Int){
-
+    fun daiBaiocchi(nBaiocchi : Int,context: Context){
+        val db = Database(context)
+        baiocchi+=nBaiocchi
+        db.aggiungiBaiocchiAPlebeo(nome,casata,nBaiocchi)
     }
 
-    fun togliBaiocchi(nBaiocchi: Int) : Boolean{
-        return false
+    fun togliBaiocchi(nBaiocchi: Int, context: Context) : Boolean{
+        if(baiocchi >= nBaiocchi ){
+            val db = Database(context)
+            baiocchi -= nBaiocchi
+            db.togliBaiocchiAPlebeo(nome,casata,nBaiocchi)
+            return true
+        } else {
+            baiocchi = 0
+            return false
+        }
     }
 
-    fun compraMiracolo(descrizione : String, santo : String){
-
+    fun compraMiracolo(descrizione : String, santo : String,context : Context){
     }
 
-    fun commentaMiracolo(descr: String, santo : String, commento : String){
-
+    fun commentaMiracolo(descr: String, santo : String, commento : String,context: Context){
+        val db = Database(context)
+        db.aggiungiCommento(descr,santo,commento)
     }
 }
