c8639ba6079716376020e21cffb1b2a21fc58c65
Zollo <francescozollo@rocketmail.com>
2022-12-18 16:52:18 +0100

    Corretto bug con ripercussioni su ID e implementato bindView
    - Francesco Zollo (18/12/2022)

--- app/src/main/java/com/example/pregaverso/AdattatoreMiracoliPlebeo.kt
+++ app/src/main/java/com/example/pregaverso/AdattatoreMiracoliPlebeo.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class AdattatoreMiracoliPlebeo(var miracoli : ArrayList<Miracolo>, private val context : Context) : RecyclerView.Adapter<AdattatoreMiracoliPlebeo.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.cardviewmiracoliplebeo,parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.cardviewmiracoloplebeo,parent, false)
         return ViewHolder(view)
     }
 
@@ -29,22 +30,53 @@ class AdattatoreMiracoliPlebeo(var miracoli : ArrayList<Miracolo>, private val c
         notifyItemRemoved(pos)
     }
 
-
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) , View.OnClickListener {
-        var descrMiracolo = itemView.findViewById(R.id.descrMiracolo) as TextView
-        var nomeSanto = itemView.findViewById(R.id.nomeSanto) as TextView
-        var costo = itemView.findViewById(R.id.nBaiocchi) as TextView
-        var testo = itemView.findViewById(R.id.testoMiracolo) as TextView
+        var descrMiracolo = itemView.findViewById(R.id.descrMiracoloPlebeo) as TextView
+        var nomeSanto = itemView.findViewById(R.id.nomeSantoPlebeo) as TextView
+        var costo = itemView.findViewById(R.id.nBaiocchiPlebeo) as TextView
+        var testo = itemView.findViewById(R.id.testoMiracoloPlebeo) as TextView
+        var titcom =  itemView.findViewById(R.id.intCommPlebeo) as TextView
+        var spiegMiracolo = itemView.findViewById(R.id.intDescrizionePlebeo) as TextView
+        var commenti = itemView.findViewById(R.id.commentiMiracoloPlebeo) as TextView
+
+        var frecciaDescr = itemView.findViewById(R.id.frecciaDescrizionePlebeo) as ImageView
+        var frecciaComm = itemView.findViewById(R.id.frecciaCommentiPlebeo) as ImageView
 
         override fun onClick(v: View?) {
             TODO("Not yet implemented")
         }
 
         fun bindView(miracolo: Miracolo) {
+
+            val db = Database(context)
+
             testo.text = miracolo.testo
             costo.text = miracolo.costo.toString()
             nomeSanto.text = miracolo.nomeSanto
             descrMiracolo.text = miracolo.descr
+
+            if (testo.text.toString() == ""){
+                frecciaDescr.visibility = View.GONE
+                spiegMiracolo.visibility = View.GONE
+                testo.visibility = View.GONE
+            }
+
+
+            val commDb = db.prendiCommento(miracolo.descr,miracolo.nomeSanto)
+
+            if(commDb.size == 0){
+                frecciaComm.visibility = View.GONE
+                titcom.visibility = View.GONE
+                commenti.visibility = View.GONE
+            } else{
+                var risultato = "0 - commento di test\n"
+
+                for(i in 0 until commDb.size){
+                    risultato += "$i - ${commDb[i]}\n"
+                }
+
+                commenti.text = risultato
+            }
         }
 
     }
