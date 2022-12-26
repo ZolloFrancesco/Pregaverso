3077c29551ae4404859aa917541e1d4515ca7b66
Zollo <francescozollo@rocketmail.com>
2022-12-26 11:30:30 +0100

    Aggiornata di commenti
    - Francesco Zollo (26/12/2022)

--- app/src/main/java/com/example/pregaverso/AdattatoreMiracoliPlebeo.kt
+++ app/src/main/java/com/example/pregaverso/AdattatoreMiracoliPlebeo.kt
@@ -23,14 +23,24 @@ class AdattatoreMiracoliPlebeo(var miracoli : ArrayList<Miracolo>, private val c
         return miracoli.size
     }
 
+    // cancella l'elemento di posizione i nella lista del recyclerView
     fun cancellaByPosition(pos: Int) {
+
         val db = Database(context)
+
+        // elimino il miracolo dal databse
         db.eliminaMiracolo(miracoli[pos].descr,miracoli[pos].nomeSanto)
+
+        // rimuovo il miracolo dalla lista del recyclerView
         miracoli.removeAt(pos)
+
+        // notifico il cambiamento al recycler
         notifyItemRemoved(pos)
     }
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) , View.OnClickListener {
+
+        // istanzio variabili riferite all'UI
         var descrMiracolo = itemView.findViewById(R.id.descrMiracoloPlebeo) as TextView
         var nomeSanto = itemView.findViewById(R.id.nomeSantoPlebeo) as TextView
         var costo = itemView.findViewById(R.id.nBaiocchiPlebeo) as TextView
@@ -49,42 +59,71 @@ class AdattatoreMiracoliPlebeo(var miracoli : ArrayList<Miracolo>, private val c
             TODO("Not yet implemented")
         }
 
+        // adatta la card al recyclerView
         fun bindView(miracolo: Miracolo) {
 
             val db = Database(context)
 
+            // inserisco i valori degli attributi di miracolo nei corrispondenti
+            // elementi grafici
             testo.text = miracolo.testo
             costo.text = miracolo.costo.toString()
             nomeSanto.text = miracolo.nomeSanto
             descrMiracolo.text = miracolo.descr
 
+            // se il testo e' vuoto
             if (testo.text.toString() == ""){
+
+                // faccio scomparire l'intera sezione della descrizione
                 frecciaDescr.visibility = View.GONE
                 spiegMiracolo.visibility = View.GONE
                 testo.visibility = View.GONE
             }
 
+            // commDb contiene tutti i commenti del miracolo
             val commDb = db.prendiCommento(miracolo.descr,miracolo.nomeSanto)
 
+            // se commDb e' vuoto
             if(commDb.size == 0){
+
+                // faccio scomparire l'intera sezione dei commenti
                 frecciaComm.visibility = View.GONE
                 titcom.visibility = View.GONE
                 commenti.visibility = View.GONE
-            } else{
+            }
+            // altrimenti
+            else{
+
+                // variabile di utilita'
                 var risultato = ""
 
+                // per ogni elemento di commDb
                 for(i in 0 until commDb.size){
+
+                    // concateno il commento
                     risultato += "$i - ${commDb[i]}\n"
                 }
 
+                // inserisco i commenti appena trovati nell'UI
                 commenti.text = risultato
             }
 
+            // listener del bottone che permette di comprare
             bottoneCompra.setOnClickListener {
+
+                // se il plebeo che ha fatto accesso puo' permettersi il miracolo
                 if(plebeoCorrente.baiocchi > costo.text.toString().toInt()){
+
+                    // elimino il miracolo
                     db.eliminaMiracolo(descrMiracolo.text.toString(),nomeSanto.text.toString())
+
+                    // tolgo i baiocchi al plebeo (nel database)
                     plebeoCorrente.togliBaiocchi(costo.text.toString().toInt(),context)
+
+                    // cancello il miracolo nel recycler
                     cancellaByPosition(adapterPosition)
+
+                    // notifico l'eliminazione del miracolo
                     notifyItemRemoved(adapterPosition)
                 }
             }
