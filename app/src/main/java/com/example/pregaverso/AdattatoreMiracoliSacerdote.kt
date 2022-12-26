11d4227b562c49161410d177b049cddc40d3702c
Zollo <francescozollo@rocketmail.com>
2022-12-26 11:30:48 +0100

    Aggiunti commenti
    - Francesco Zollo (26/12/2022)

--- app/src/main/java/com/example/pregaverso/AdattatoreMiracoliSacerdote.kt
+++ app/src/main/java/com/example/pregaverso/AdattatoreMiracoliSacerdote.kt
@@ -25,6 +25,7 @@ class AdattatoreMiracoliSacerdote(var miracoli : ArrayList<Miracolo>, private va
         return miracoli.size
     }
 
+    // cancella l'elemento pos-esimo dalla recyclerView
     fun cancellaByPosition(pos: Int) {
         val db = Database(context)
         db.eliminaMiracolo(miracoli[pos].descr,miracoli[pos].nomeSanto)
@@ -33,6 +34,8 @@ class AdattatoreMiracoliSacerdote(var miracoli : ArrayList<Miracolo>, private va
     }
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) , View.OnClickListener {
+
+        // variabili per riferirsi all'UI
         var descrMiracolo = itemView.findViewById(R.id.descrMiracoloSacerdote) as TextView
         var nomeSanto = itemView.findViewById(R.id.nomeSantoSacerdote) as TextView
         var costo = itemView.findViewById(R.id.nBaiocchiSacerdote) as TextView
@@ -53,31 +56,45 @@ class AdattatoreMiracoliSacerdote(var miracoli : ArrayList<Miracolo>, private va
         fun bindView(miracolo: Miracolo) {
             val db = Database(context)
 
+            // riempio la card con i valori dell'attributo del miracolo
             testo.text = miracolo.testo
             costo.text = miracolo.costo.toString()
             nomeSanto.text = miracolo.nomeSanto
             descrMiracolo.text = miracolo.descr
 
+            // inserisco i valori degli attributi di miracolo nei corrispondenti
+            // elementi grafici
             if (testo.text.toString() == ""){
                 frecciaDescr.visibility = GONE
                 spiegMiracolo.visibility = GONE
                 testo.visibility = GONE
             }
 
-
+            // commDb contiene tutti i commenti del miracolo
             val commDb = db.prendiCommento(miracolo.descr,miracolo.nomeSanto)
 
+            // se commDb e' vuoto
             if(commDb.size == 0){
+
+                // faccio scomparire l'intera sezione dei commenti
                 frecciaComm.visibility = GONE
                 titcom.visibility = GONE
                 commenti.visibility = GONE
-            } else{
-                var risultato = "0 - commento di test\n"
+            }
+            // altrimenti
+            else{
 
+                // variabile di utilita'
+                var risultato = ""
+
+                // per ogni elemento di commDb
                 for(i in 0 until commDb.size){
+
+                    // concateno il commento
                     risultato += "$i - ${commDb[i]}\n"
                 }
 
+                // inserisco i commenti appena trovati nell'UI
                 commenti.text = risultato
             }
         }
